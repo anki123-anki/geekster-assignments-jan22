e64ecef181b64f5ce51008847a39a554abdd4101
anki123-anki <56147228+anki123-anki@users.noreply.github.com>
2022-03-06 17:39:58 +0530

    Add files via upload

---
+++ January/week4/Assignment (25-01-2022)/index2.js
@@ -0,0 +1,18 @@
+                                                  //Take 3 numbers and write a program to find the maximum of those 3 numbers.
+
+var a=5;
+var b=15;
+var c=8;
+
+if(a>b && a>c)
+{
+console.log("a is maximum");
+}
+else if(b>a && b>c)
+{
+console.log("b is maximum");
+}
+else
+{
+console.log("c is maximum");
+}
